7b8357cf5961d8c23ce48c413d1123d19de22154
alex03g <alexchania132@gmail.com>
2025-02-28 21:42:42 +0200

    Conditional description box in admin interface

--- TESTDJANGOAPP/newproject/myapp/admin.py
+++ TESTDJANGOAPP/newproject/myapp/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
+from .models import Task, Post
 
 # Register your models here.
 
-from .models import Task
 admin.site.register(Task)
 
-from .models import Post
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    def get_fields(self, request, obj=None):
+        fields = super().get_fields(request, obj)
+        
+        # if object exists and content isnt checked, remove the description box
+        if obj and not obj.content:
+            fields.remove('description')
+        
+        return fields
+
+admin.site.register(Post, PostAdmin)
